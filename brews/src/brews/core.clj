fb388745c2f795349080771ebe65585b9b7dc4e3
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-07-27 00:24:53 -0400

    Chapter 8

---
+++ brews/src/brews/core.clj
@@ -0,0 +1,56 @@
+(ns brews.core)
+
+(def order-details-validations
+  {:name
+   ["Please enter a name" not-empty]
+
+   :email
+   ["Please enter an email address" not-empty
+
+    "Your email address doesn't look like an email address"
+    #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
+(defn validate
+  "Returns a map with a vector of errors for each key"
+  [to-validate validations]
+  (reduce (fn [errors validation]
+            (let [[fieldname validation-check-groups] validation
+                  value (get to-validate fieldname)
+                  error-messages (error-messages-for value validation-check-groups)]
+              (if (empty? error-messages)
+                errors
+                (assoc errors fieldname error-messages))))
+          {}
+          validations))
+
+(defmacro if-valid
+  "Handle validation more concisely"
+  [to-validate validations errors-name & then-else]
+  `(let [~errors-name (validate ~to-validate ~validations)]
+     (if (empty? ~errors-name)
+       ~@then-else)))
+
+(defmacro when-valid
+  [to-validate validations & then]
+  `(let [errors# (validate ~to-validate ~validations)]
+     (when (empty? errors#)
+       ~@then)))
+
+(defmacro my-or
+  ([] nil)
+  ([x] x)
+  ([x & xs]
+   `(let [x# ~x]
+      (if x# x# (my-or ~@xs)))))
+
+(defmacro defattrs
+  [& pairs]
+  `(do ~(map (fn [[attr-fn-name attr-key]]
+                   (def ~attr-fn-name (comp ~attr-key :attributes)))
+                 (partition 2 pairs))))
